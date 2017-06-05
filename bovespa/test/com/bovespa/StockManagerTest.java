be7af26321b7fe7dd4f207a9da211068a6899ecb
WillianCruzUniville <willian.cruz@univille.br>
2017-06-05 15:01:33 -0300

    bovespa

---
+++ bovespa/test/com/bovespa/StockManagerTest.java
@@ -0,0 +1,19 @@
+package com.bovespa;
+
+import java.io.IOException;
+import org.junit.Test;
+
+public class StockManagerTest {
+
+	@Test
+	public void loadStocksTest() throws IOException{
+		StockManager stockManager = new StockManager();
+		Stock[] stocks = stockManager.loadStocks("C:/eclipseneon2/COTAHIST_A2016.txt");
+		
+		for (Stock stock : stocks) {
+			if(stock.getCode().equals("PETR4") && stock.getDate().getDayOfMonth() == 21 & stock.getDate().getMonthValue() == 3 & stock.getDate().getYear() == 2016) {
+				System.out.println(stock.getOpenPrice());
+			}
+		}
+	}	
+}
